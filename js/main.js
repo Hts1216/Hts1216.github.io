3834e893c834e3a67ebd21006f4175a8800d8332
Hts1216 <3250913221@qq.com>
2025-05-01 18:41:31 +0800

    Site updated: 2025-05-01 18:41:31

--- js/main.js
+++ js/main.js
@@ -151,7 +151,7 @@ var selectRandomSong = [];
 // 音乐默认声音大小
 var musicVolume = 0.7;
 // 是否切换了周杰伦音乐列表
-var changeMusicListFlag = true;
+var changeMusicListFlag = false;
 // 当前默认播放列表
 var defaultPlayMusicList = [];
 var themeColorMeta, pageHeaderEl, navMusicEl, consoleEl;
